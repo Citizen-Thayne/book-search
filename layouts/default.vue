edf0089d16f275089cec76426ee2cddeb8808ff2
Ryan Wrigley <rwrigley@Ryans-MBP.attlocal.net>
2017-12-22 21:42:26 -0600

    Cleaned up unused parts of layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,6 @@
 <template>
-  <v-app dark>
+  <v-app>
     <v-navigation-drawer
-      :mini-variant="miniVariant"
-      :clipped="clipped"
       v-model="drawer"
       fixed
       app
@@ -24,56 +22,17 @@
         </v-list-tile>
       </v-list>
     </v-navigation-drawer>
-    <v-toolbar fixed app :clipped-left="clipped">
-      <v-toolbar-side-icon @click="drawer = !drawer"></v-toolbar-side-icon>
-      <v-btn 
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"></v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>web</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>remove</v-icon>
-      </v-btn>
+    <v-toolbar fixed app color="blue">
+      <v-toolbar-side-icon @click="drawer = !drawer"></v-toolbar-side-icon>       
       <v-toolbar-title v-text="title"></v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>menu</v-icon>
-      </v-btn>
     </v-toolbar>
     <v-content>
       <v-container>
         <nuxt />
       </v-container>
-    </v-content>
-    <v-navigation-drawer
-      temporary
-      :right="right"
-      v-model="rightDrawer"
-      fixed
-    >
-      <v-list>
-        <v-list-tile @click.native="right = !right">
-          <v-list-tile-action>
-            <v-icon light>compare_arrows</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer :fixed="fixed" app>
+    </v-content>   
+    <v-footer :fixed="false" app color="blue">
       <span>&copy; 2017</span>
     </v-footer>
   </v-app>
@@ -83,16 +42,14 @@
   export default {
     data () {
       return {
-        clipped: false,
         drawer: true,
-        fixed: false,
         items: [
-          { icon: 'search', title: 'Search Books', to: '/' }
+          { icon: 'search', title: 'Search', to: '/' },
+          { icon: 'book', title: 'Collections', to: '/collections' }
         ],
-        miniVariant: false,
         right: true,
         rightDrawer: false,
-        title: 'Vuetify.js'
+        title: 'vue-books'
       }
     }
   }
